401aa860fd2a89a3eb5004f29e817039135a20b9
mvidyasagarreddy <mvsreddy2018@gmail.com>
2023-06-28 05:11:39 +0530

    Frontend Script

---
+++ mongodb.sh
@@ -0,0 +1,11 @@
+echo -e "\e[33mCopying the repo file \e[0m"
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+
+echo -e "\e[33mInstalling MongoDB server\e[0m"
+yum install mongodb-org -y &>>/tmp/roboshop.log
+
+#Modify the config file
+
+echo -e "\e[33m \e[0m"
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
