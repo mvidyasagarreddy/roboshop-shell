d6a97c692854de2ee726ff4bf90f2f6e8d4a96d2
mvidyasagarreddy <mvsreddy2018@gmail.com>
2023-06-28 04:21:34 +0530

    Adding messages for readability in frontend script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # roboshop-shell
+
+echo -e "\[33m \e[0m"
--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,19 @@
+echo -e "\[33mInstalling Nginx\e[0m"
 yum install nginx -y
 
+echo -e "\[33mRemoving old App content \e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\[33mDownloading Frontend Content \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\[33mExtract Frontend Content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 #vim /etc/nginx/default.d/roboshop.conf
 
+
+echo -e "\[33mStarting Nginx Server\e[0m"
 systemctl enable nginx
 systemctl restart nginx
