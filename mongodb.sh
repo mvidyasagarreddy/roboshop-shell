8ee95a9344966452765817ddc80c923fa206eafa
mvidyasagarreddy <mvsreddy2018@gmail.com>
2023-07-28 17:31:14 +0530

    Mongodb Script sed

--- mongodb.sh
+++ mongodb.sh
@@ -4,7 +4,7 @@ cp mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling MongoDB server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log &>>/tmp/roboshop.log
 
-echo -e "\Update MongoDB listen address[0m"
+echo -e "\e[33mUpdate MongoDB listen address[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 
 echo -e "\e[33m Start MongoDB Service\e[0m"
