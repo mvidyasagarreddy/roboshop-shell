0721ee2e51b6d801b45142bef5aa93dfa776f71a
mvidyasagarreddy <mvsreddy2018@gmail.com>
2023-08-12 10:07:27 +0530

    Updated RabbitMq Script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -8,11 +8,10 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/sc
 echo -e "\e[33mInstall RabbitMQ \e[0m"
 yum install rabbitmq-server -y &>>/tmp/roboshop.lo
 
-echo -e "\e[33mRabbitMQ comes with a default username / password as guest/guest. But this user cannot be used to connect. Hence, we need to create one user for the application. \e[0m"
-rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.lo
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.lo
-
-
 echo -e "\e[33mStart RabbitMQ Service\e[0m"
 systemctl enable rabbitmq-server &>>/tmp/roboshop.lo
 systemctl restart rabbitmq-server &>>/tmp/roboshop.lo
+
+echo -e "\e[33mRabbitMQ comes with a default username / password as guest/guest. But this user cannot be used to connect. Hence, we need to create one user for the application. \e[0m"
+rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.lo
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.lo
