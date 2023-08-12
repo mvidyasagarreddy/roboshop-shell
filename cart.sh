0b9792bb1978e5e8ea3058aec912cd0cd12a195b
mvidyasagarreddy <mvsreddy2018@gmail.com>
2023-08-12 17:01:41 +0530

    NFR

--- cart.sh
+++ cart.sh
@@ -1,35 +1,4 @@
 source common.sh
 component=cart
 
-
-echo -e "${color}  Configuring NodeJS Repos ${no_color}  "
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${log_file}
-
-echo -e "${color}  Install NodeJS ${no_color}  "
-yum install nodejs -y  &>>${log_file}
-
-echo -e "${color}  Add Application User ${no_color}  "
-useradd roboshop  &>>${log_file}
-
-echo -e "${color}  Create Application Directory  ${no_color}  "
-rm -rf ${app_path}  &>>${log_file}
-mkdir ${app_path}
-
-echo -e "${color}  Download Application Content ${no_color}  "
-curl -o /tmp/${component}  .zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>${log_file}
-cd ${app_path}
-
-echo -e "${color}  Extract Application Content ${no_color}  "
-unzip /tmp/${component}.zip  &>>${log_file}
-cd ${app_path}
-
-echo -e "${color}  Install NodeJS Dependencies ${no_color}  "
-npm install  &>>${log_file}
-
-echo -e "${color}  Setup SystemD Service   ${no_color}  "
-cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service  &>>${log_file}
-
-echo -e "${color}  Start ${component} Service  ${no_color}  "
-systemctl daemon-reload  &>>${log_file}
-systemctl enable ${component}  &>>${log_file}
-systemctl restart ${component}  &>>${log_file}
+nodejs
--- common.sh
+++ common.sh
@@ -2,3 +2,47 @@ color="\e[35m"
 nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
+
+nodejs(){
+  echo -e "${color} Configuring NodeJs Repositories ${nocolor}"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+  echo -e "${color} Installing NodeJs  ${nocolor}"
+  yum install nodejs -y &>>${log_file}
+
+  echo -e "${color} Adding Roboshop user  ${nocolor}"
+  useradd roboshop &>>${log_file}
+
+  echo -e "${color} Create the application directory  ${nocolor}"
+  rm -rf ${app_path} &>>${log_file}
+  mkdir ${app_path}
+
+  echo -e "${color} Download Application content  ${nocolor}"
+  curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
+  cd ${app_path}
+
+  echo -e "${color} Extract the application content  ${nocolor}"
+  unzip /tmp/$component.zip &>>${log_file}
+
+  echo -e "${color} Install nodejs Dependencies  ${nocolor}"
+  npm install &>>${log_file}
+
+  echo -e "${color} Setup systemd service ${nocolor}"
+  cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service
+
+  echo -e "${color} Start $component service  ${nocolor}"
+  systemctl daemon-reload &>>${log_file}
+  systemctl enable $component &>>${log_file}
+  systemctl restart $component &>>${log_file}
+}
+
+mongodb(){
+  echo -e "${color} Copy mongodb repo file  ${nocolor}"
+  cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+
+  echo -e "${color} Install mongodb client  ${nocolor}"
+  yum install mongodb-org-shell -y &>>${log_file}
+
+  echo -e "${color} Load Schema  ${nocolor}"
+  mongo --host mongodb-dev.roboshopai.online <${app_path}/schema/$component.js &>>${log_file}
+}
