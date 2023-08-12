30164a08866c65ff8943edb23ff048674097c779
mvidyasagarreddy <mvsreddy2018@gmail.com>
2023-08-12 17:25:54 +0530

    NFR

--- common.sh
+++ common.sh
@@ -1,19 +1,13 @@
 color="\e[35m"
-nocolor="\e[0m"
+nocolor=" ${no_color}"
 log_file="/tmp/roboshop.log"
-app_path="/app"
+app_path="${app_path}"
 
-nodejs(){
-  echo -e "${color} Configuring NodeJs Repositories ${nocolor}"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-
-  echo -e "${color} Installing NodeJs  ${nocolor}"
-  yum install nodejs -y &>>${log_file}
-
+app_presetup(){
   echo -e "${color} Adding Roboshop user  ${nocolor}"
   useradd roboshop &>>${log_file}
 
-  echo -e "${color} Create the application directory  ${nocolor}"
+  echo -e "${color} Creating App Directory  ${no_color}"
   rm -rf ${app_path} &>>${log_file}
   mkdir ${app_path}
 
@@ -22,11 +16,11 @@ nodejs(){
   cd ${app_path}
 
   echo -e "${color} Extract the application content  ${nocolor}"
+  cd /app
   unzip /tmp/$component.zip &>>${log_file}
+}
 
-  echo -e "${color} Install nodejs Dependencies  ${nocolor}"
-  npm install &>>${log_file}
-
+systemd_setup(){
   echo -e "${color} Setup systemd service ${nocolor}"
   cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service
 
@@ -34,6 +28,21 @@ nodejs(){
   systemctl daemon-reload &>>${log_file}
   systemctl enable $component &>>${log_file}
   systemctl restart $component &>>${log_file}
+}
+
+nodejs(){
+  echo -e "${color} Configuring NodeJs Repositories ${nocolor}"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+  echo -e "${color} Installing NodeJs  ${nocolor}"
+  yum install nodejs -y &>>${log_file}
+
+  app_presetup
+
+  echo -e "${color} Install nodejs Dependencies  ${nocolor}"
+  npm install &>>${log_file}
+
+  systemd_setup
 }
 
 mongodb(){
@@ -45,4 +54,28 @@ mongodb(){
 
   echo -e "${color} Load Schema  ${nocolor}"
   mongo --host mongodb-dev.roboshopai.online <${app_path}/schema/$component.js &>>${log_file}
+}
+mysql_schema_setup(){
+  echo -e "${color} Installing Mysql  ${no_color}"
+  yum install mysql -y &>>${log_file}
+
+  echo -e "${color} Load Schema ${nocolor}"
+  mysql -h mysql-dev.devopsb73.store -uroot -p${mysql_root_password} </app/schema/${component}.sql   &>>$log_file
+}
+maven(){
+  echo -e "${color} Installing Maven  ${no_color}"
+  yum install maven -y &>>${log_file}
+
+  app_presetup
+
+  echo -e "${color} Clean Package  ${no_color}"
+  mvn clean package &>>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+
+  echo -e "${color} Copying the ${component} service file m  ${no_color}"
+  cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+
+  mysql_schema_setup
+
+  systemd_setup
 }
